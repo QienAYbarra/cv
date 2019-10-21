c0c7f274dd46db9e18b3aaa60bc9a0470600627e
QienAYbarra <54871593+QienAYbarra@users.noreply.github.com>
2019-10-20 21:08:51 -0500

    Added a Dog Age Converter

---
+++ dogAgeConverter.js
@@ -0,0 +1,7 @@
+var dogAge = prompt("What's your dog's age?");
+
+var humanAge = ((dogAge - 2) * 4) + 21;
+
+var convertedAge = humanAge;
+
+alert("Your dog is approximately " + convertedAge + " years old as a human.")
